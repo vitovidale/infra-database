3020e4636c8d87ef648ffb6acdc9b448290d8ec2
Victor Vidal <vicemanoel@hotmail.com>
2025-03-12 14:02:26 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,14 +31,22 @@ variable "db_password" {
   default     = "AlterarSenhaAqui"
 }
 
+###########################################################
+# Hardcoded: VPC, SG e Subnets
+###########################################################
 locals {
-  vpc_id       = "vpc-035823898b0432060"  # Hardcoded para sua VPC
-  sg_id        = "sg-0b32fbeb948914196"    # SG existente; certifique-se de que permite acesso na porta 5432
+  vpc_id       = "vpc-035823898b0432060"           # Sua VPC
+  sg_id        = "sg-0b32fbeb948914196"             # Seu Security Group (deve permitir a porta 5432)
   subnet_ids   = ["subnet-0e8a9c57e24921ad2", "subnet-054f5e7046e524dc7"]
+  # Seleciona o DB Subnet Group existente ou o criado
   db_subnet_name = length(data.aws_db_subnet_group.existing_db_subnet.id) > 0 ? data.aws_db_subnet_group.existing_db_subnet.id : aws_db_subnet_group.fastfood_db_subnet[0].name
+  # Seleciona o Secret existente ou o criado
   secret_id      = length(data.aws_secretsmanager_secret.existing_db_password_secret.id) > 0 ? data.aws_secretsmanager_secret.existing_db_password_secret.id : aws_secretsmanager_secret.db_password_secret[0].id
 }
 
+###########################################################
+# DB Subnet Group
+###########################################################
 data "aws_db_subnet_group" "existing_db_subnet" {
   name = "fastfood-db-subnet"
 }
@@ -53,6 +61,9 @@ resource "aws_db_subnet_group" "fastfood_db_subnet" {
   }
 }
 
+###########################################################
+# Secret para o DB
+###########################################################
 data "aws_secretsmanager_secret" "existing_db_password_secret" {
   name = "fastfood-db-password"
 }
@@ -67,7 +78,10 @@ resource "aws_secretsmanager_secret_version" "db_password_version" {
   secret_string = var.db_password
 }
 
-# Consulta IGW existente usando o filtro correto "attachment.vpc-id"
+###########################################################
+# Internet Gateway e IGW efetivo
+###########################################################
+# Consulta se já existe um IGW na VPC
 data "aws_internet_gateway" "existing_igw" {
   filter {
     name   = "attachment.vpc-id"
@@ -75,6 +89,7 @@ data "aws_internet_gateway" "existing_igw" {
   }
 }
 
+# Cria um novo IGW se não existir
 resource "aws_internet_gateway" "new_igw" {
   count  = length(data.aws_internet_gateway.existing_igw.id) > 0 ? 0 : 1
   vpc_id = local.vpc_id
@@ -87,23 +102,23 @@ locals {
   effective_igw_id = length(data.aws_internet_gateway.existing_igw.id) > 0 ? data.aws_internet_gateway.existing_igw.id : aws_internet_gateway.new_igw[0].id
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = local.vpc_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = local.effective_igw_id
-  }
-  tags = {
-    Name = "fastfood-public-rt"
-  }
+###########################################################
+# Atualizar a tabela de rotas padrão com uma rota para o IGW
+###########################################################
+data "aws_route_table" "default" {
+  default = true
+  vpc_id  = local.vpc_id
 }
 
-resource "aws_route_table_association" "public_subnet_association" {
-  count          = length(local.subnet_ids)
-  subnet_id      = local.subnet_ids[count.index]
-  route_table_id = aws_route_table.public_rt.id
+resource "aws_route" "default_route_to_igw" {
+  route_table_id         = data.aws_route_table.default.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = local.effective_igw_id
 }
 
+###########################################################
+# Instância RDS (PostgreSQL) PUBLICAMENTE ACESSÍVEL
+###########################################################
 resource "aws_db_instance" "fastfood_db" {
   allocated_storage     = 20
   storage_type          = "gp2"
@@ -116,7 +131,7 @@ resource "aws_db_instance" "fastfood_db" {
   password              = var.db_password
   publicly_accessible   = true
   skip_final_snapshot   = true
-  vpc_security_group_ids = [ local.sg_id ]
+  vpc_security_group_ids = [local.sg_id]
   db_subnet_group_name   = local.db_subnet_name
   tags = {
     Project     = "FastFood"
@@ -124,11 +139,13 @@ resource "aws_db_instance" "fastfood_db" {
   }
   depends_on = [
     aws_internet_gateway.new_igw,
-    aws_route_table.public_rt,
-    aws_route_table_association.public_subnet_association
+    aws_route.default_route_to_igw
   ]
 }
 
+###########################################################
+# Output: Endpoint do RDS
+###########################################################
 output "db_endpoint" {
   description = "Endpoint do RDS (PostgreSQL)"
   value       = aws_db_instance.fastfood_db.endpoint
